501c06da2403c3643a81bc35491818d5118470c0
Eli_Santiago_Manzano <67016286+EliStgoManz@users.noreply.github.com>
2023-03-14 17:53:13 -0600

    Update AsheActionsCallbacks.java

--- src/semana1y2/AsheActionsCallbacks.java
+++ src/semana1y2/AsheActionsCallbacks.java
@@ -1,13 +1,13 @@
 package semana1y2;
 
-/*
+
 public interface AsheActionsCallbacks {
     /*Aqui realizamos la interfas-
      ¿que es lo que hace? */
 
- /*   void frostShot(Screen s);
+   void frostShot(Screen s);
     void hawShot(Screen s);
     void rangerFocus(Screen s);
     void volley(Screen s);
     void crystalArrow(Screen s);
-}*/
+}
